ecfdc586fded08613b47154807fb1b613834c8a6
jdleoblue <jdleoblue@icloud.com>
2026-05-22 20:48:54 -0400

    Restore adapter after disabling API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://byjdg-cms.pages.dev",
-  output: "static"
+  output: "server",
+  adapter: cloudflare()
 });
