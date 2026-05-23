9ab33e39508e73e895e55407b8e137eeffe222aa
jdleoblue <jdleoblue@icloud.com>
2026-05-22 20:42:41 -0400

    Make beta deployment fully static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://byjdg-cms.pages.dev",
-  output: "server",
-  adapter: cloudflare()
+  output: "static"
 });
